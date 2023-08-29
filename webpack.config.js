e622ace95219a795f0bedd814766a67b5caca1ce
Ayokunnumi1 <omololuayk@gmail.com>
2023-08-29 16:30:18 +0100

    Fix webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-     clean: true,
-    },
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-        title: 'Output Management',
-        template: './src/index.html',
+      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
   module: {
